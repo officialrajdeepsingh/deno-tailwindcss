ec020fd39d4a9643bcb9eeb24b238fca873fc013
officialrajdeepsingh <radewala.2@gmail.com>
2022-12-27 16:14:19 +0530

    update the counter css

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -8,8 +8,8 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   const [count, setCount] = useState(props.start);
   return (
-    <div >
-      <p>{count}</p>
+    <div>
+      <p className="my-5 text-blue-800 text-center text-2xl ">{count}</p>
       <Button onClick={() => setCount(count - 1)}>-1</Button>
       <Button onClick={() => setCount(count + 1)}>+1</Button>
     </div>
