70830ed819e96cba641852d50fe2ac15bea510cd
officialrajdeepsingh <radewala.2@gmail.com>
2022-12-27 17:05:39 +0530

    update [name].tsx

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,5 +1,10 @@
 import { PageProps } from "$fresh/server.ts";
+import Layout from '../components/Layout.tsx';
 
 export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+  return <Layout>
+              <div className="mx-auto mt-16">
+                <h2 className="text-center text-4xl">Hello {props.params.name}</h2>
+                </div>
+    </Layout>
 }
